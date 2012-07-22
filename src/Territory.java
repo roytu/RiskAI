851f9dabbcac38ab250d1e06ce3cbb258f315646
Roy Tu <thatsnail@gmail.com>
2012-07-22 13:23:48 -0700

    Git ignore

--- src/Territory.java
+++ src/Territory.java
@@ -1,4 +1,14 @@
+import java.util.*;
 
 public class Territory {
-
+	protected List<Territory> linkedTerritories;
+	public String name;
+	public Territory()
+	{
+		linkedTerritories = new ArrayList<Territory>();
+	}
+	public static void initializeTerritories()
+	{
+		new Territory();
+	}
 }
