56c421b94978fe9970bdde0ccd52468e597ee0b4
je007 <jezerozeroseven@gmail.com>
2012-07-22 21:17:49 -0700

    forgot to save player.java...

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -52,6 +53,48 @@ public class Player {
 	public int getUnitsInTerritory(Territory territory)
 	{
 		return unitMap.get(territory);
+	}
+	public void attack(Territory from,Territory to)
+	{
+		int unitsFrom = getUnitCount();
+		int unitsTo = to.getUnitCount();
+		
+		int countDiceFrom = Math.min(unitsFrom-1, 3);
+		int countDiceTo = Math.min(unitsTo, 2);
+		
+		List<Integer> diceFromList = new ArrayList<Integer>();
+		List<Integer> diceToList = new ArrayList<Integer>();
+		
+		Random random = new Random();
+		for(int i=0;i<countDiceFrom;++i)
+		{
+			//TODO: Roll attacking dice
+			diceFromList.add(random.nextInt(6)+1);
+		}
+		for(int i=0;i<countDiceTo;++i)
+		{
+			//TODO: Roll defending dice
+			diceToList.add(random.nextInt(6)+1);
+		}
+		
+		Collections.sort(diceFromList);
+		Collections.reverse(diceFromList);
+		Collections.sort(diceToList);
+		Collections.reverse(diceToList);
+		
+		for(int i=0;i<Math.min(countDiceFrom, countDiceTo);++i)
+		{
+			if(countDiceFrom > countDiceTo)
+			{
+				//attacker wins
+				to.getOwner().addToTerritory(to, -1);
+			}
+			else
+			{
+				//defender wins
+				getOwner().addToTerritory(this, -1);
+			}
+		}
 	}
 
 }
