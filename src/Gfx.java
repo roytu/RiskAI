e029a6933f30da0d5af29e5af95f424d59d05f49
Roy Tu <thatsnail@gmail.com>
2012-07-24 19:18:18 -0700

    Reversed Roy's crappy GUI stuff

--- src/Gfx.java
+++ src/Gfx.java
@@ -1,10 +1,7 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 
@@ -13,7 +10,6 @@ public class Gfx extends JFrame{
 	private static final int SCREEN_WIDTH = 1200;
 	private static final int SCREEN_HEIGHT = 582;
 	
-	Graphics2D g2;
 	public Gfx()
 	{
 		setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -26,10 +22,7 @@ public class Gfx extends JFrame{
 
 	public void paint(Graphics g)
 	{
-		ImageIcon img = new ImageIcon(this.getClass().getResource("bkg.jpg")).getImage();
 		
-		g2 = (Graphics2D) g;
-		g2.drawImage(new BufferedImage("bkg.jpg"), 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, null);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
