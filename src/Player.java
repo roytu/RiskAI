21661389321bb9f6b143fadb41fb267a655d2f0b
je007 <jezerozeroseven@gmail.com>
2012-07-26 14:32:33 -0700

    found null territory errors

--- src/Player.java
+++ src/Player.java
@@ -54,6 +54,7 @@ public abstract class Player {
 		{
 			unitMap.put(territory, number);
 		}
+		System.out.println(territory.name+" reinforced");
 	}
 	
 	public void attack(Territory from, Territory to) 
