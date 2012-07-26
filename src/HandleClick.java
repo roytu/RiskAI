c3f9a8faedbe720a2757baaa8573350d7f61b5f5
Ben <ben@huchley.com>
2012-07-26 14:17:12 -0700

    added clicking on territories

--- src/HandleClick.java
+++ src/HandleClick.java
@@ -1,16 +1,17 @@
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.List;
 
 
 public class HandleClick implements MouseListener {
 	
-	public static enum ClickStates {
-		EDIT_MAP,
-		MOVE_HUMAN,
-		MOVE_COMPUTER
-	};
-	
-	private static ClickStates clickState;
+//	public static enum ClickStates {
+//		EDIT_MAP,
+//		MOVE_HUMAN,
+//		MOVE_COMPUTER
+//	};
+//	
+//	private static ClickStates clickState;
 	
 	/*
 	static boolean mouseHasClicked = true;//this and line below
@@ -20,42 +21,55 @@ public class HandleClick implements MouseListener {
 
 	public HandleClick()
 	{
-		clickState = ClickStates.MOVE_HUMAN;
+		//clickState = ClickStates.MOVE_HUMAN;
 	}
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
-		switch(clickState)
+//		switch(clickState)
+//		{
+//		case EDIT_MAP:
+//			/*
+//			if(mouseHasClicked==false && allTerritoriesDone==false)
+//			{
+//				RiskAI.territoryData.get(territoryIndex).setCoordinates(e.getX(), e.getY());
+//				System.out.println("Coordinates for "+RiskAI.territoryData.get(territoryIndex).name+" set.");
+//				territoryIndex++;
+//				if(territoryIndex == RiskAI.territoryData.size())
+//				{
+//					allTerritoriesDone = true;
+//					for (Territory t : RiskAI.territoryData)
+//					{
+//						t.writeToFile();
+//					}
+//				}
+//			mouseHasClicked = true;
+//			}
+//			if(mouseHasClicked == true && allTerritoriesDone == false)
+//			{
+//				System.out.println(RiskAI.territoryData.get(territoryIndex).name);
+//				mouseHasClicked=false;
+//			}
+//			 */
+//			break;
+//		case MOVE_HUMAN:
+//			
+//			break;
+//		}
+		Territory clickedOn = findTerritoryByPosition(e.getX(),e.getY());
+		if (clickedOn != null) RiskAI.currentGame.onClick(clickedOn); //implement this method
+	}
+	
+	private Territory findTerritoryByPosition(int x, int y)
+	{
+		List<Territory> territoryData = RiskAI.territoryData;
+		for (Territory t : territoryData)
 		{
-		case EDIT_MAP:
-			/*
-			if(mouseHasClicked==false && allTerritoriesDone==false)
-			{
-				RiskAI.territoryData.get(territoryIndex).setCoordinates(e.getX(), e.getY());
-				System.out.println("Coordinates for "+RiskAI.territoryData.get(territoryIndex).name+" set.");
-				territoryIndex++;
-				if(territoryIndex == RiskAI.territoryData.size())
-				{
-					allTerritoriesDone = true;
-					for (Territory t : RiskAI.territoryData)
-					{
-						t.writeToFile();
-					}
-				}
-			mouseHasClicked = true;
-			}
-			if(mouseHasClicked == true && allTerritoriesDone == false)
-			{
-				System.out.println(RiskAI.territoryData.get(territoryIndex).name);
-				mouseHasClicked=false;
-			}
-			 */
-			break;
-		case MOVE_HUMAN:
-			
-			break;
+			TerritoryGraphics graphic = t.getTerritoryGraphic();
+			if (graphic.icon.contains(x, y)) return t;
 		}
+		return null;
 	}
 
 	@Override
