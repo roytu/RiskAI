e72358aaf787cf68f584885cb522891fe901c759
Ben <ben@huchley.com>
2012-07-31 13:41:56 -0700

    fixed some more ajacents and modifed gameoverexception

--- src/GameOverException.java
+++ src/GameOverException.java
@@ -1,4 +1,6 @@
-
-public class GameOverException extends Exception{
+//It now extends error rather than exception so that we can't accidentally catch it by catching exception e.
+//Error is basically identical to exception but nothing will automatically catch them, since they don't extend exception.
+//They are basically designed to be uncatchable exceptions, so that seems like it would be useful here.
+public class GameOverException extends Error{
 	
 }
