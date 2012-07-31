84b39c87eea5fcdfad891bd74722babb0d5abf13
Ben <ben@huchley.com>
2012-07-31 14:36:27 -0700

    cleared random import warnings

--- src/Gfx.java
+++ src/Gfx.java
@@ -7,7 +7,6 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
